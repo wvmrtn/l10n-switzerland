f01ad11055d2f458100f36cd30f21fd3d88fe7df
William Martin <wmartin@compassion.ch>
2020-06-19 14:29:12 +0200

    CO-3268 fix code

--- l10n_ch_pain_direct_debit/models/move_line.py
+++ l10n_ch_pain_direct_debit/models/move_line.py
@@ -72,29 +72,12 @@ class AccountMoveLine(models.Model):
                     if mandate.state == 'valid':
                         return bank.id
 
-    # @api.multi
-    # def _prepare_payment_line_vals(self, payment_order):
-    #     vals = super()._prepare_payment_line_vals(payment_order)
-        # if self.invoice_id:
-        #     if self.invoice_id.reference_type == 'none':
-        #         if 'out' in self.invoice_id.type:
-        #             vals['communication'] = self.move_id.ref
-        #             vals['communication'] = vals['communication'].replace(' ', '')
-
-        # communication = self.move_id.ref or self.move_id.name
-        # # change these default values if move line is linked to an invoice
-        # if self.invoice_id:
-        #     if self.invoice_id.reference_type != 'none':
-        #         communication = self.invoice_id.reference
-        #     else:
-        #         if (
-        #                 self.invoice_id.type in ('in_invoice', 'in_refund') and
-        #                 self.invoice_id.reference):
-        #             communication = self.invoice_id.reference
-        #         elif 'out' in self.invoice_id.type:
-        #             # Force to only put invoice number here
-        #             communication = self.move_id.ref
-        # vals['communication'] = communication
-        # vals['communication'] = vals['communication'].replace(' ', '')
-
-        # return vals
+    @api.multi
+    def _prepare_payment_line_vals(self, payment_order):
+        vals = super()._prepare_payment_line_vals(payment_order)
+        if self.invoice_id:
+            if self.invoice_id.reference_type == 'none':
+                if 'out' in self.invoice_id.type:
+                    vals['communication'] = self.move_id.ref
+                    vals['communication'] = vals['communication'].replace(' ', '')
+        return vals
